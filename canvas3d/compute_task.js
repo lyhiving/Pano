0d60e891a9677b5347ebe520e2f92ebc07d993e0
Yu Lingjie <ylj1123456@163.com>
2013-05-11 15:49:53 +0800

    finish rendering with web workers . time consumption is less than 200 ms, about 150 ms on average

---
+++ canvas3d/compute_task.js
@@ -0,0 +1,101 @@
+function setMatrix(_tilt,_pan)
+{
+		var mt1=new Float32Array(3*3);
+		var mt2=new Float32Array(3*3);
+		mt1[0]=1;
+		mt1[1]=0;
+		mt1[2]=0;
+		mt1[3]=0;
+		mt1[4]=Math.cos(_tilt*Math.PI/180);
+		mt1[5]=Math.sin(_tilt*Math.PI/180);
+		mt1[6]=0;
+		mt1[7]=-mt1[5];
+		mt1[8]=mt1[4];
+		mt2[0]=Math.cos(_pan*Math.PI/180);
+		mt2[1]=0;
+		mt2[2]=-Math.sin(_pan*Math.PI/180);
+		mt2[3]=0;
+		mt2[4]=1;
+		mt2[5]=0;
+		mt2[6]=-mt2[2];
+		mt2[7]=0;
+		mt2[8]=mt2[0];
+		var mt=new Float32Array(3*3);
+		var i=0;
+		var j=0;
+		for(i=0;i!=3;i++)
+		{
+			for(j=0;j!=3;j++)
+			{
+				mt[i*3+j]=mt1[i*3+0]*mt2[0*3+j]+mt1[i*3+1]*mt2[1*3+j]+mt1[i*3+2]*mt2[2*3+j];
+					
+			}
+		}
+		return mt
+
+}
+
+onmessage=function(event)
+{
+	var data=event.data;
+	var tilt=data.tilt;
+	var pan=data.pan;
+	var fov=data.fov;
+	var view=data.view;
+	var vwidth=parseInt(data.width);
+	var vheight=parseInt(data.height);
+	var pwidth=parseInt(data.pwidth);
+	var pheight=parseInt(data.pheight);
+	var r=data.radius;
+	var ti=0,tj=0,tk=0;
+	var x=0,y=0,z=0;
+	var u=0,v=0;
+	var pano=new Uint16Array(vheight*vwidth*2);
+	var mt=setMatrix(tilt,pan);
+	for(var i=0;i!=vheight;++i)
+	{
+		for(var j=0;j!=vwidth;++j)
+		{
+			
+
+			ti=view[(i*vwidth+j)*3+0];
+			tj=view[(i*vwidth+j)*3+1];
+			tk=view[(i*vwidth+j)*3+2];
+
+			x=ti*mt[0*3+0]+tj*mt[1*3+0]+tk*mt[2*3+0];
+			y=ti*mt[0*3+1]+tj*mt[1*3+1]+tk*mt[2*3+1];
+			z=ti*mt[0*3+2]+tj*mt[1*3+2]+tk*mt[2*3+2];
+
+			
+			if(z>=0)
+			{ 
+				u=r*Math.acos(x/Math.sqrt(z*z+x*x));
+				v=r*(Math.PI/2-Math.atan(y/Math.sqrt(x*x+z*z)));
+			
+			}
+			else if(z<0)
+			{
+				u=r*(Math.PI*2-Math.acos(x/Math.sqrt(z*z+x*x)));
+				v=r*(Math.PI/2-Math.atan(y/Math.sqrt(x*x+z*z)));
+					
+			}
+			
+		
+			
+			if(u>=pwidth)
+			{
+				u-=pwidth;
+			}
+			if(u<0)
+			{
+				u+=pwidth;
+			}
+		
+			u=parseInt(u);
+			v=parseInt(v);
+			pano[(i*vwidth+j)*2]=u;
+			pano[(i*vwidth+j)*2+1]=v;
+		}
+	}
+	self.postMessage({pano:pano},[pano.buffer]);
+}
--- canvas3d/panorama.js
+++ canvas3d/panorama.js
@@ -11,7 +11,14 @@
 	var pwidth;
 	var pheight;
 	var srcMat;
-
+	var imgMat;
+	//Workers
+	var workers=[];
+	var jobCount=2;
+	var flag=jobCount;
+	
+	var timbeg;
+	var timend;
 	function Mat(_row,_col,_data,_buffer)
 	{
 		this.row=_row||0;
@@ -82,82 +89,7 @@
 		imageData.data.set(_imgMat.data);
 		return imageData;
 	}
-	function backProjectionAlgorithm(_x,_y)
-	{
 
-		var f=vwidth/(2*Math.tan(fov*Math.PI/360));
-		var a=tilt*Math.PI/180;
-		var b=pan*Math.PI/180;
-		var w=(_y-vheight/2)*Math.sin(a)*Math.cos(b)-(_x-vwidth/2)*Math.sin(b)-f*Math.cos(a)*Math.cos(b);
-		
-		var u=(_x-vwidth/2)*Math.cos(b)-f*Math.cos(a)*Math.sin(b);
-		var v=(_y-vheight/2)*Math.cos(a)+f*Math.sin(a);	
-		var sq=Math.sqrt(u*u+w*w);
-		var x1=0,y1=0;
-		if(w>=0)
-		{
-			x1=f*Math.acos(u/sq);
-			y1=f*(Math.PI/2-Math.atan(v/sq));
-		}
-		else if(w<0)
-		{
-			x1=f*(Math.PI*2-Math.acos(u/sq));
-			y1=f*(Math.PI/2-Math.atan(v/sq));
-		}
-		var  ad=new Array();
-		ad[0]=x1;
-		ad[1]=y1;
-		return ad;
-
-	}
-	function drawOne()
-	{
-		var imgMat=new Mat(vheight,vwidth);
-		var data=imgMat.data;
-		var srcData=srcMat.data;
-		var ti=0,tj=0;
-		var u=0,v=0;
-		var ad;
-		for(var i=0;i!=vheight;++i)
-		{
-			for(var j=0;j!=vwidth;++j)
-			{
-			
-				ti=-i+(vheight/2);
-				tj=j-(vwidth/2);
-				ti--;
-				ad=backProjectionAlgorithm(tj,ti);
-				u=parseInt(ad[0]);
-				v=parseInt(ad[1]);
-				
-				if(u>pwidth)
-				{
-					u-=pwidth;
-				}
-				if(u<0)
-				{
-					u+=pwidth;
-				}
-				if(srcData[(v*pwidth+u)*4]==0)
-				{
-						console.log("z");
-				}
-				for(var k=0;k!=4;++k)
-				{
-					data[(i*vwidth+j)*4+k]=srcData[(v*pwidth+u)*4+k];
-				}
-			}
-		}
-		var imgData=RGBA2ImageData(imgMat);
-		//draw on the canvas
-		context.clearRect(0,0,vwidth,vheight);
-		canvas.width=vwidth;
-		canvas.height=vheight;
-		canvas.style.width=vwidth;
-		canvas.style.height=vheight;
-		context.putImageData(imgData,0,0);	
-
-	}
 	function draw()
 	{
 		//transform view position to pan tilt angle;
@@ -166,123 +98,105 @@
 		var i=0,j=0,k=0;
 		var mt=setMatrix(tilt,pan);
 
-	//	var r=vwidth/(2*Math.asin(fov*Math.PI/360));
 		var r=pwidth/(2*Math.PI);
-		//x y z float array
-		var view=new Float32Array(vwidth*vheight*3);	
+	
 		var tk=-r;
 		var ti=0,tj=0;
-		
-		for(i=0;i!=vheight;++i)
-		{
-			for(j=0;j!=vwidth;++j)
-			{
-				ti=-i+(vheight/2);
-				tj=j-(vwidth/2);	
-				ti--;
-				for(k=0;k!=3;++k)
-				{
-					view[(i*vwidth+j)*3+k]=tj*mt[0*3+k]+
-					ti*mt[1*3+k]+
-					tk*mt[2*3+k];
-				}
-			
-
-			}
-		}
-		//find correct position in panorama
-		var u=0,v=0;
 		var x=0,y=0,z=0;
-		var uMax=0;
-		var panoPos=new Float32Array(vheight*vwidth*2);
-		for(i=0;i!=vheight;i++)
-		{
-			for(j=0;j!=vwidth;++j)
-			{
-				x=view[(i*vwidth+j)*3+0];
-				y=view[(i*vwidth+j)*3+1];
-				z=view[(i*vwidth+j)*3+2];
-				if(Math.abs(z)<0.00001)
-				{
-					uMax=r*Math.atan(x/z);
-				}
-				
-				if(z>=0)
-				{ 
-					u=r*Math.acos(x/Math.sqrt(z*z+x*x));
-					v=r*(Math.PI/2-Math.atan(y/Math.sqrt(x*x+z*z)));
-				
-				}
-				else if(z<0)
-				{
-					u=r*(Math.PI*2-Math.acos(x/Math.sqrt(z*z+x*x)));
-					v=r*(Math.PI/2-Math.atan(y/Math.sqrt(x*x+z*z)));
-						
-				}
-				u--;
-				
-				v++;
-			//	v=pheight/2-v;
-				if(v>=pheight)
-				{
-					v=pheight-1;
-				}
-				if(u>=pwidth)
-				{
-					u-=pwidth;
-				}
-				if(u<0)
-				{
-					u+=pwidth;
-				}
-				panoPos[(i*vwidth+j)*2+0]=u;
-				panoPos[(i*vwidth+j)*2+1]=v;
-
-			}	
-		}	
-		//get RGBA	
-
+		var u=0,v=0;
+	
 		var imgMat=new Mat(vheight,vwidth);
 		var data=imgMat.data;
 		var srcData=srcMat.data;
-		 
-		for( i=0;i!=vheight;++i)
+		var view;
+		
+		for(k=0;k!=jobCount;++k)
 		{
-			for( j=0;j!=vwidth;++j)
+			view=new Float32Array(vheight/jobCount*vwidth*3);
+			for(i=0;i!=vheight/jobCount;++i)
 			{
-				u=panoPos[(i*vwidth+j)*2];
-				v=panoPos[(i*vwidth+j)*2+1];
-				u=parseInt(u);
-				v=parseInt(v);
-				for(var k=0;k!=4;k++)
+			
+				for(j=0;j!=vwidth;++j)
 				{
-					data[(i*vwidth+j)*4+k]=srcData[(v*pwidth+u)*4+k];
+				
+				view[(i*vwidth+j)*3]=j-(vwidth/2);	
+
+				view[(i*vwidth+j)*3+1]=-(i+k*vheight/jobCount)+(vheight/2)-1;
+				view[(i*vwidth+j)*3+2]=-r;	
+		
 				}
 			}
+			workers[k].postMessage(
+					{
+						tilt:tilt,
+						pan:pan,
+						fov:fov,
+						view:view,
+						height:vheight/jobCount,
+						width:vwidth,
+						radius:r,
+						pwidth:pwidth,
+						pheight:pheight
+					},[view.buffer]
+					);
+			
+		
+			
 		}
-		imgMat.data.set(data);
-		//transform imgMat to imgData
-		var imgData=RGBA2ImageData(imgMat);
+		//	timbeg=(new Date()).getTime();
+		
+	}	
+function createWorker(i,imgData,srcData)
+{
+	
+	 workers[i]=new Worker("compute_task.js");
+	 workers[i].onmessage=function(event){
+	 flag--;
+	 var u=0,v=0;
+	 var pano_pos=event.data.pano;
+	 for(var j=0;j!=vheight/jobCount;++j)
+         {
+  		 for(var k=0;k!=vwidth;++k)
+		 {
+		 	u=pano_pos[(j*vwidth+k)*2];
+		 	v=pano_pos[(j*vwidth+k)*2+1];
+		 	for(var l=0;l!=4;++l)
+	 		{
+				imgData[((j+vheight/jobCount*i)*vwidth+k)*4+l]=
+					srcData[(v*pwidth+u)*4+l];
+			}
+		 }
+	}
+	 if(flag==0)
+	 {
+	 	var img=RGBA2ImageData(imgMat);
 		//draw on the canvas
 		context.clearRect(0,0,vwidth,vheight);
 		canvas.width=vwidth;
 		canvas.height=vheight;
 		canvas.style.width=vwidth;
 		canvas.style.height=vheight;
-		context.putImageData(imgData,0,0);
-	}	
+		context.putImageData(img,0,0);
+		flag=jobCount;
+	//	 timend=(new Date()).getTime();
+		 
+	//		console.log(timend-timbeg);
+	 }
+	};
+}
 	$.fn.panorama=function(){
 		this.each(function(){
 			 vwidth=600;
 			  vheight=400;
 			  pwidth=0;
 			  pheight=0;
-			 fov=90;
+			 fov=90; 
 			 pan=90;
-			 tilt=-90;
+			 tilt=0;
+			
 			 
-			var pano_mouse_standard_x=vwidth/2;
-			var pano_mouse_standard_y=vheight/2;
+			var pano_mouse_position_x=vwidth/2;
+			var pano_mouse_position_y=vheight/2;
 			var pano_mouse_delta_x=0;
 			var pano_mouse_delta_y=0;
 			var pano_mouse_down=false;
@@ -302,14 +216,28 @@
 				pwidth=panoImg.width;
 				pheight=panoImg.height;
 				srcMat=imread(panoImg);
-				var timebegin=(new Date()).getTime();
+				
+				
+				 imgMat=new Mat(vheight,vwidth);
+				 var imgData=imgMat.data;
+				 var srcData=srcMat.data;
+				 
+				
+				 
+				 for( var i=0;i!=jobCount;++i)
+			    	 {
+					createWorker(i,imgData,srcData);
+				 }
+				
+				
+				 
 				
 				draw();
-				var timeend=(new Date()).getTime();
-				alert(timeend-timebegin);
+				
 				$(pano_element).bind('mousedown',function(event){
 				pano_mouse_down=true;
-				
+				pano_mouse_position_x=event.clientX;
+				pano_mouse_position_y=event.clientY;
 				$(pano_element).parent().css("cursor","move");
 				
 				
@@ -321,34 +249,20 @@
 			});
 			$(pano_element).bind('mousemove',function(event){
 				if(pano_mouse_down){
-					pano_mouse_delta_x=pano_mouse_standard_x-event.clientX;
-					pano_mouse_delta_y=pano_mouse_standard_y-event.clientY;
+					pano_mouse_delta_x=pano_mouse_position_x-event.clientX;
+					pano_mouse_delta_y=pano_mouse_position_y-event.clientY;
 					
-					if(pano_mouse_delta_x >0)
-					{
-						pan+=1;
-						
-					}
-					else if(pano_mouse_delta_x<0)
-					{
-						pan-=1;
-					}
-					if(pano_mouse_delta_y >0)
-					{
-						tilt+=1;
+					pan+=pano_mouse_delta_x/100;
+					
+					
+						tilt+=pano_mouse_delta_y/100;
 						if(tilt>90)
 							tilt=90;
-					}
-					else if(pano_mouse_delta_y<0)
-					{
-						tilt-=1;
 						if(tilt<-90)
-							{
-								tilt=-90;
-							}
-					}
+							tilt=-90;
 					
 					draw();
+				
 				}
 			});
 			}
